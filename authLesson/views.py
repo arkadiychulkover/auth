56f8a552b7d3e613914d3f564a1dc2e4beee84ef
Arkadii <arkadiychulkov11@gmail.com>
2026-05-17 17:57:05 +0300

    init

---
+++ authLesson/views.py
@@ -0,0 +1,74 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout
+from django.http import HttpResponseNotAllowed, HttpRequest
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import AuthenticationForm
+from .forms import RegistrationForm
+
+def register_page(request: HttpRequest):
+    return render(request, "register.html", {"form": RegistrationForm()})
+ 
+def register_view(request: HttpRequest):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("auth_page")
+        else:
+            return render(request, "register.html", {"form": form})
+    return HttpResponseNotAllowed(["POST"])
+
+
+def login_page(request: HttpRequest):
+    if request.user.is_authenticated:
+        return redirect("auth_page")
+    return render(request, "register.html", {"form": AuthenticationForm(), "button": "Login", "action": 'login_view'})
+
+
+def login_view(request: HttpRequest):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('auth_page')
+        else:
+            return render(request, "register.html", {"form": form, "button": "Login", "action": 'login_view'})
+    return HttpResponseNotAllowed(['POST'])
+
+
+def logout_view(request: HttpRequest):
+    logout(request)
+    return redirect('login_page')
+
+
+@login_required
+def auth_page(request: HttpRequest):
+    return render(request, 'auth.html', {'user': request.user})
+
+
+@login_required
+def create_user(request: HttpRequest):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("auth_page")
+    else:
+        form = RegistrationForm()
+    return render(request, "base_form.html", {"form": form, "button": "Create User"})
+
+
+@login_required
+def create_manager(request: HttpRequest):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.is_staff = True
+            user.save()
+            return redirect("auth_page")
+    else:
+        form = RegistrationForm()
+    return render(request, "base_form.html", {"form": form, "button": "Create Manager"})
---
+++ config/urls.py
@@ -0,0 +1,15 @@
+from django.conf.urls.static import static
+from django.urls import path
+
+from authLesson import views
+
+urlpatterns = [
+    path("signup/", views.register_page, name="register_page"),
+    path("login/", views.login_page, name="login_page"),
+    path("logout/", views.logout_view, name="logout"),
+    path("auth/", views.auth_page, name="auth_page"),
+    path("newuser/", views.register_view, name="new_user"),
+    path("login-view/", views.login_view, name="login_view"),
+    path("create-user/", views.create_user, name="create_user"),
+    path("create-manager/", views.create_manager, name="create_manager"),
+]
